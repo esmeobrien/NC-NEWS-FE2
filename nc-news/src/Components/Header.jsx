5f856eca46442ac094b1690726d9b11172cce9b3
Esmé O`Brien <esmeobrien@esmes-mbp.home>
2019-03-22 21:12:59 +0000

    sorted going back to home page

--- nc-news/src/Components/Header.jsx
+++ nc-news/src/Components/Header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Header = ({ user }) => (
   <header>
-    <h1 className="text-center">MY NC NEWS</h1>
+    <a href="/">
+      <h1 className="text-center">MY NC NEWS</h1>
+    </a>
     <h4 className="logged">Logged in as: {user} </h4>
   </header>
 );
